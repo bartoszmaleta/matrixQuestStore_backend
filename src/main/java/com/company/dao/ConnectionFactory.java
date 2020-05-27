df93f3047d776cfd458add6366d65acede3b2e8a
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-05-27 18:48:10 +0200

    Add ConnectionFactory + readUserByEmailAndPassword

--- src/main/java/com/company/dao/ConnectionFactory.java
+++ src/main/java/com/company/dao/ConnectionFactory.java
@@ -1,5 +1,64 @@
 package com.company.dao;
 
+import java.sql.*;
+
 public class ConnectionFactory {
-    
-}
+        private String databaseUrl;
+        private String jdbcDriver;
+
+        public ConnectionFactory() {
+            this.databaseUrl = "jdbc:sqlite:src/main/resources/sqliteDB/AllteregoDB";
+            this.jdbcDriver = "org.sqlite.JDBC";
+        }
+
+        protected Connection con;
+        protected Statement stmt = null;
+        protected ResultSet rs = null;
+
+        public Connection getConnection() {
+            try {
+                Class.forName(this.jdbcDriver);
+                this.con = DriverManager.getConnection(this.databaseUrl);
+            } catch (SQLException e) {
+                System.out.println("Error! Cannot connect with the database." );
+            } catch (ClassNotFoundException e) {
+                System.out.println("Error! Cannot find JDBC Driver!");
+            }
+            return this.con;
+        }
+
+        public ResultSet executeQuery(String sql) {
+            ConnectionFactory dbsqlite = new ConnectionFactory();
+            this.con = dbsqlite.getConnection();
+            try {
+                this.stmt = this.con.createStatement();
+                this.rs = this.stmt.executeQuery(sql);
+            } catch (SQLException e) {
+                System.out.println("Error! Cannot execute query!");
+                e.printStackTrace();
+            }
+            return this.rs;
+        }
+
+        public void updateQuery(String sql) {
+            ConnectionFactory connectionFactory = new ConnectionFactory();
+            this.con = connectionFactory.getConnection();
+            try {
+                stmt = con.createStatement();
+                stmt.executeQuery(sql);
+            } catch (SQLException e) {
+                System.out.println("Error! Cannot update query!");
+                e.printStackTrace();
+            }
+        }
+
+        public void close() throws SQLException {
+            try {
+                this.stmt.close();
+                this.con.close();
+            }catch (SQLException e) {
+                System.out.println("Error! Can't connect with the database." );
+            }
+        }
+
+    }
--- src/main/java/com/company/dao/UserDaoDb.java
+++ src/main/java/com/company/dao/UserDaoDb.java
@@ -2,8 +2,37 @@ package com.company.dao;
 
 import com.company.models.users.User;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+
 public class UserDaoDb {
     public User readUserByEmailAndPassword() {
+
+        Connection c = null;
+        User newUser = new User();
+        try {
+            // TODO: why twice prints this:???
+//            System.out.println("\nI am in readUserByNameAndPassword\n");
+            ConnectionFactory ds = new ConnectionFactory();
+            ResultSet rs = ds.executeQuery("SELECT * FROM Users WHERE \"Name\" = '"+userName+"' AND \"Password\" = '"+userPassword+"';");
+            if (rs.next() && rs.getString("Name").equals(userName)) {
+                int id = rs.getInt("Id");
+                String name = rs.getString("Name");
+                String password = rs.getString("Password");
+                String email = rs.getString("Email");
+                int isAdmin = rs.getInt("IsAdmin");
+
+                newUser.setId(id);
+                newUser.setName(name);
+                newUser.setPassword(password);
+                newUser.setEmail(email);
+                newUser.setIsAdmin(isAdmin);
+            }
+            rs.close();
+        } catch (Exception e) {
+            System.err.println("Error! Reading user by userName and userPassword from DB failed!");
+        }
+        return newUser;
         return null;
     }
 }
