d31db01a4ff7b9c8085770f9f3e174487141e46c
Ozukyo <ozukyo@gmail.com>
2020-05-28 14:30:05 +0200

    delete my previous DbConnector, change connecting with DB in questDao

---
+++ src/main/java/com/company/dao/QuestDAO.java
@@ -0,0 +1,114 @@
+package com.company.dao;
+
+import com.company.models.Quest;
+import org.postgresql.core.ConnectionFactory;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class QuestDAO {
+    ArrayList<Quest> listOfQuests;
+
+    public QuestDAO(DBConnector dbConnector) {
+        conFactory = new ConnectionFactory();
+    }
+
+    public void addQuest(Quest quest) {
+        PreparedStatement ps = null;
+        try {
+            ps = conFactory.getConnection().prepareStatement("INSERT INTO \"Quests\" (title, description, coins, image, mentor_id)" +
+                    "VALUES (?, ?, ?, ?, ?);");
+            ps.setString(1, quest.getTitle());
+            ps.setString(2, quest.getDescription());
+            ps.setInt(3, quest.getPrice());
+            ps.setString(4, quest.getImageSrc());
+            ps.setInt(5, quest.getMentorId());
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateQuest(Quest quest) {
+
+    }
+
+    public void deleteQuestById(int id) {
+        PreparedStatement ps = null;
+
+        try {
+            ps = conFactory.getConnection().prepareStatement("DELETE FROM \"Quests\" WHERE id =" + id + ";");
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<Quest> readQuestList() {
+        listOfQuests = new ArrayList<>();
+        try {
+            ResultSet rs = conFactory.executeQuery("SELECT * FROM \"Quests\";");
+            while(rs.next()) {
+                int id = rs.getInt("id");
+                String title = rs.getString("title");
+                String description = rs.getString("description");
+                int price = rs.getInt("coins");
+                String imageSrc = rs.getString("image");
+                int mentorId = rs.getInt("mentor_id");
+
+                listOfQuests.add(new Quest(id, title, description, price, imageSrc, mentorId));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return listOfQuests;
+    }
+
+    public void readAllQuestsOrderById() {
+        try {
+            ResultSet rs = conFactory.executeQuery("SELECT * FROM \"Quests\" ORDER BY id;");
+            while(rs.next()) {
+                int id = rs.getInt("id");
+                String title = rs.getString("title");
+                String description = rs.getString("description");
+                int price = rs.getInt("coins");
+                String imgSrc = rs.getString("image");
+                Timestamp mentorId = rs.getTimestamp("mentor_id");
+
+                String format = "|%1$-4s|%2$-25s|%3$-70s|%4$-7s|%5$-10s|%6$-25s\n";
+                System.out.printf(format, id, title, description, price, imgSrc, mentorId);
+            }
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void readAllQuestsOrderByAmountOfCoins() {
+        try {
+            ResultSet rs = conFactory.executeQuery("SELECT * FROM \"Quests\" ORDER BY coins;");
+            while(rs.next()) {
+                int id = rs.getInt("id");
+                String title = rs.getString("title");
+                String description = rs.getString("description");
+                int price = rs.getInt("coins");
+                String imgSrc = rs.getString("image");
+                Timestamp mentorId = rs.getTimestamp("mentor_id");
+
+                String format = "|%1$-4s|%2$-25s|%3$-70s|%4$-7s|%5$-10s|%6$-25s\n";
+                System.out.printf(format, id, title, description, price, imgSrc, mentorId);
+            }
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void viewQuests(ArrayList<Quest> listOfQuests) {
+        for(Quest quest: listOfQuests) {
+            System.out.println(quest);
+        }
+    }
+}
