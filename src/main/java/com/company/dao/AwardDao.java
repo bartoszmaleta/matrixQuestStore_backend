57b530aa857962b35345a389b5f9cb5a7847bccb
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-06-21 23:29:30 +0200

    Add AwardDao interface

--- src/main/java/com/company/dao/AwardDao.java
+++ src/main/java/com/company/dao/AwardDao.java
@@ -1,4 +1,28 @@
 package com.company.dao;
 
+import com.company.models.Award;
+import com.company.models.users.User;
+
+import java.util.List;
+
 public interface AwardDao extends Dao {
+    void updateAwardCreatorIdById(int id, int creatorId);
+    void updateAwardPriceById(int id, int price);
+    void updateAwardDescriptionById(int id, String description);
+    void updateAwardTitleById(int id, String title);
+    void addAward(Award award);
+    void deleteAwardById(int id);
+    List<Award> readAwardListByMentor(User user);
+    List<Award> readAwardListWithMentors();
+
+
+
+
+
+
+
+
+
+
+
 }
