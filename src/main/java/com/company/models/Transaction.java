81aa82cf3464e2b2c488936702258a888cf81b0d
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-06-22 01:30:30 +0200

    Add Transaction class

--- src/main/java/com/company/models/Transaction.java
+++ src/main/java/com/company/models/Transaction.java
@@ -1,4 +1,39 @@
 package com.company.models;
 
 public class Transaction {
+    private int transactionId;
+    private final String buyer;
+    private final String date;
+    private final String artifactName;
+
+    public Transaction(String buyer, String date, String artifactName) {
+        this.buyer = buyer;
+        this.date = date;
+        this.artifactName = artifactName;
+    }
+    public Transaction(int transactionId, String buyer, String date, String artifactName) {
+        this(buyer, date, artifactName);
+        this.transactionId = transactionId;
+    }
+
+    public int getTransactionId() {
+        return transactionId;
+    }
+
+    public Transaction setTransactionId(int transactionId) {
+        this.transactionId = transactionId;
+        return this;
+    }
+
+    public String getBuyer() {
+        return buyer;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getArtifactName() {
+        return artifactName;
+    }
 }
