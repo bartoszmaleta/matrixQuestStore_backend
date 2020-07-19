630c7691d4d36e6a6b531ad29c19d303037fa570
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-07-19 15:32:29 +0200

    Refactor

--- src/main/java/com/company/helpers/Parsers.java
+++ src/main/java/com/company/helpers/Parsers.java
@@ -1,17 +1,17 @@
 package com.company.helpers;
 
-import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Parsers {
-    public static Map<String, String> parseFormData(String formData) throws UnsupportedEncodingException {
+    public static Map<String, String> parseFormData(String formData) {
         Map<String, String> map = new HashMap<>();
         String[] pairs = formData.split("&");
         for (String pair : pairs) {
             String[] keyValue = pair.split("=");
-            String value = new URLDecoder().decode(keyValue[1], "UTF-8");
+            String value = URLDecoder.decode(keyValue[1], StandardCharsets.UTF_8);
             map.put(keyValue[0], value);
         }
         return map;
