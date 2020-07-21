32849bace69e073000b898f631dc185705953392
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-07-21 19:59:04 +0200

    Add test of createing user

--- src/test/java/com/company/dao/UserDaoDbTest.java
+++ src/test/java/com/company/dao/UserDaoDbTest.java
@@ -2,19 +2,25 @@ package com.company.dao;
 
 import com.company.model.user.Student;
 import com.company.model.user.User;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class UserDaoDbTest {
-    ConnectionFactory connectionFactory = new ConnectionFactory(
-            "jdbc:postgresql://ec2-54-217-206-236.eu-west-1.compute.amazonaws.com/da8tt4mh63b7nc"
-            , "org.postgresql.Driver"
-            , "pirqathgcgzhbg"
-            , "15c50442ada3956b30448ed4f67f2ec081ffedc990ade3019893a9d6b51655ed"
-    );
-    UserDao userDao = new UserDaoDb(connectionFactory);
+public class UserDaoDbTest {
+    ConnectionFactory connectionFactory;
+    UserDao userDao;
 
+    @BeforeAll
+    public void setUp() {
+        connectionFactory = new ConnectionFactory(
+                "jdbc:postgresql://ec2-54-217-206-236.eu-west-1.compute.amazonaws.com/da8tt4mh63b7nc"
+                , "org.postgresql.Driver"
+                , "pirqathgcgzhbg"
+                , "15c50442ada3956b30448ed4f67f2ec081ffedc990ade3019893a9d6b51655ed"
+        );
+        userDao = new UserDaoDb(connectionFactory);
+    }
 
     @Test
     public void should_returnUser_when_idIsProvidedId() {
--- src/test/java/com/company/model/user/UserTest.java
+++ src/test/java/com/company/model/user/UserTest.java
@@ -3,14 +3,28 @@ package com.company.model.user;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class UserTest {
 
     @Test
     public void should_throwException_when_emailIsNotProvided() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-                User nUser = new Student("name", "surname", "login", "pass", "", 3, "../resources/avatars/loginLogo.jpg");
+                User newUser = new Student("name", "surname", "login", "pass", "", 3, "../resources/avatars/loginLogo.jpg");
         });
+    }
+
+    @Test
+    public void should_createUser_when_fieldsSetByBuilder() {
+        User newUser = new Student()
+                .setName("Added")
+                .setSurname("User")
+                .setLogin("addUser")
+                .setPassword("pass")
+                .setEmail("addedUser@")
+                .setRoleEnum(3);
+        assertEquals("Added", newUser.getName());
     }
 
 }
