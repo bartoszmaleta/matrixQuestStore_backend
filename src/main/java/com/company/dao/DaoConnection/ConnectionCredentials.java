57afb4222951c55b321077620a9414ed857a4128
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-07-22 18:53:19 +0200

    Add class with Credentials

--- src/main/java/com/company/dao/DaoConnection/ConnectionCredentials.java
+++ src/main/java/com/company/dao/DaoConnection/ConnectionCredentials.java
@@ -2,9 +2,9 @@ package com.company.dao.DaoConnection;
 
 public class ConnectionCredentials {
     // TODO:
-    public final String DATABASE_URL = "jdbc:postgresql://ec2-54-246-85-151.eu-west-1.compute.amazonaws.com:5432/dcmgt3tfcp4n6o";
-    public final String JDBC_DRIVER = "org.postgresql.Driver";
-    public final String USER_LOGIN = "tilcavmrsuhbzj";
-    public final String USER_PASSWORD = "37e3925b366710ece9a679ad72d401e74bc6bb4ed1239676aaffef00ed27fc52";
+    public static final String DATABASE_URL = "jdbc:postgresql://ec2-54-246-85-151.eu-west-1.compute.amazonaws.com:5432/dcmgt3tfcp4n6o";
+    public static final String JDBC_DRIVER = "org.postgresql.Driver";
+    public static final String USER_LOGIN = "tilcavmrsuhbzj";
+    public static final String USER_PASSWORD = "37e3925b366710ece9a679ad72d401e74bc6bb4ed1239676aaffef00ed27fc52";
 
 }
